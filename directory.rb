a44debc7c5a28d577721429fd7792cb3d204f4e9
Pablo <pabloandresvdl@gmail.com>
2017-09-12 23:36:01 +0100

    add load_students method to keep students in a file

--- directory.rb
+++ directory.rb
@@ -1,13 +1,13 @@
-@students = [] # Empty array available for all methods 
+@students = [] # Empty array available for all methods
 
 def interactive_menu
 
   loop do
     # 1. print the menu and ask the user what to do
-    print_menu 
-    # 2. read the input and save it into a variable  
+    print_menu
+    # 2. read the input and save it into a variable
     # 3. do what the user has asked
-    process(gets.chomp) 
+    process(gets.chomp)
   end
 end
 
@@ -19,25 +19,28 @@ def process(selection)
   show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
     puts "I don't know what you meant, try again"
   end
-end 
+end
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
-end 
+end
 
 def show_students
   print_header
   print_students_list
   print_footer
-end 
+end
 
 def input_students
 
@@ -47,164 +50,171 @@ def input_students
   # students = []
   # get the first name
   name = gets.delete!("\n")
-  
-  # while the name is not empty, repeat this code      
-while !name.empty?    
-  # Prompting the user to enter a cohort 
+
+  # while the name is not empty, repeat this code
+while !name.empty?
+  # Prompting the user to enter a cohort
     puts "For which cohort ?"
-    cohort = gets.delete!("\n") 
-# Checking if cohort is a valid month 
+    cohort = gets.delete!("\n")
+# Checking if cohort is a valid month
 while (cohort.downcase.match(/(\bjanuary\b|\bfebruary\b|\bmarch\b|\bapril\b|\bmay\b|\bjune\b|\bjuly\b|\baugust\b|\bseptember\b|\boctober\b|\bnovember\b|\bdecember\b)/)) == nil do
-    # exiting loop if cohort is empty 
+    # exiting loop if cohort is empty
       puts "The default cohort will be November " if cohort.empty?
       break                                       if cohort.empty?
-    
+
     # If the user provides a cohort which is not a valid month, then we prompt the user to enter a cohort again
       puts "Your response was #{cohort}"
       puts "It seems that there is a typo "
       puts "Please write the month without abbreviation"
       puts "--------------------"
-      puts "Please type cohort again"    
-      cohort = gets.delete!("\n")  
+      puts "Please type cohort again"
+      cohort = gets.delete!("\n")
     end
 
 # We exit the above loop when cohort is empty or when cohort has a valid month
     cohort = cohort.capitalize.to_sym
-    cohort = :November if cohort.empty?   # Turning cohort into a symbol 
-    
+    cohort = :November if cohort.empty?   # Turning cohort into a symbol
+
     # add the student hash to the array
-    @students << {name: name, cohort: cohort, hobbies: :coding, country: :UK, height: 1.75} 
+    @students << {name: name, cohort: cohort, hobbies: :coding, country: :UK, height: 1.75}
     puts "Now we have #{@students.count} students" if @students.count > 1
     puts "Now we have #{@students.count} student" if @students.count == 1
-    # get another name from the user  
-    puts "Please enter another student name "     
-    puts "To finish, just hit return twice"       
-    
-    name = gets.delete!("\n")  
+    # get another name from the user
+    puts "Please enter another student name "
+    puts "To finish, just hit return twice"
+
+    name = gets.delete!("\n")
   end
 
- return @students.reject{ |x| x[:name].length > 12} 
- 
+ return @students.reject{ |x| x[:name].length > 12}
 
-end 
+
+end
 
 
 
 def print_header
   puts "The students of Villains Academy"
-  puts "--------------" 
-end 
+  puts "--------------"
+end
 
 
 def print_students_list
-  
-    
-    if !@students.empty? 
-    # Asking if they want to filter the names by the first letter 
+
+
+    if !@students.empty?
+    # Asking if they want to filter the names by the first letter
     puts "Do you want to see the students with a name that starts with a specific letter?"
     puts "Please respond Yes or No."
     user_response = gets.delete!("\n")
-    
-    # checking the response provided by the user : must be yes or no. Anything else is not valid 
+
+    # checking the response provided by the user : must be yes or no. Anything else is not valid
     while (user_response.downcase.match(/^[yes|no]+$/)) == nil do
       puts "Your response was #{user_response}"
-      puts "Please answer Yes or No. No other response is accepted"    
-      user_response = gets.delete!("\n")  
-    end    
-    
-      
+      puts "Please answer Yes or No. No other response is accepted"
+      user_response = gets.delete!("\n")
+    end
+
+
   # return @students.reject{ |x| x[:name].length > 12}
-  
+
    if user_response.downcase == "no"
      newArr = @students.group_by {|x| x[:cohort]}
      newArr.each do |cohort, arr|
-       countStudent = arr.length   
-       index = 0 
-       puts "**************************".center(50)   
+       countStudent = arr.length
+       index = 0
+       puts "**************************".center(50)
        puts "Cohort for #{cohort.upcase} ".center(50)
        puts "**************************".center(50)
-       while countStudent >0                
+       while countStudent >0
             newIndex = "#{index}".to_i
          puts "#{index+1}. #{arr[newIndex][:name]}".center(50)
-         puts "(#{arr[newIndex][:cohort]} cohort)".center(60)  
-         puts "Hobbies : #{arr[newIndex][:hobbies]}, Height: #{arr[newIndex][:height]}, from #{arr[newIndex][:country]} ".center(60)    
+         puts "(#{arr[newIndex][:cohort]} cohort)".center(60)
+         puts "Hobbies : #{arr[newIndex][:hobbies]}, Height: #{arr[newIndex][:height]}, from #{arr[newIndex][:country]} ".center(60)
          puts " -----------------------------------------------------------"
-           countStudent = countStudent - 1 
-           index = index + 1       
-       end 
+           countStudent = countStudent - 1
+           index = index + 1
+       end
      end
    elsif  user_response.downcase == "yes"
-     # if user says "yes" then we need to ask for the letter 
-    puts "Which letter?" if user_response.downcase == "yes" 
-   
+     # if user says "yes" then we need to ask for the letter
+    puts "Which letter?" if user_response.downcase == "yes"
+
    letter = gets.delete!("\n")
-   
-   # check whether the letter is a valid alphabet letter...nothing else 
-   # Also we need to check if the input is only ONE letter 
-   while (letter.length != 1 || letter[/[a-z]|[A-Z]/] == nil) do 
+
+   # check whether the letter is a valid alphabet letter...nothing else
+   # Also we need to check if the input is only ONE letter
+   while (letter.length != 1 || letter[/[a-z]|[A-Z]/] == nil) do
      puts "Only ONE letter of the ALPHABET is accepted "
      puts "No other character is allowed "
-     letter = gets.delete!("\n") 
+     letter = gets.delete!("\n")
    end
-     
+
      newArr = @students
      .select{ |x| x[:name][0].downcase == letter.downcase }
      .reject{ |x| x[:name].length > 12}
      .group_by {|x| x[:cohort]}
      newArr.each do |cohort, arr|
-       countStudent = arr.length   
-       index = 0 
-       puts "**************************".center(50)   
+       countStudent = arr.length
+       index = 0
+       puts "**************************".center(50)
        puts "Cohort for #{cohort.upcase} ".center(50)
        puts "**************************".center(50)
-       while countStudent >0                
+       while countStudent >0
             newIndex = "#{index}".to_i
          puts "#{index+1}. #{arr[newIndex][:name]}".center(50)
-         puts "(#{arr[newIndex][:cohort]} cohort)".center(60)  
-         puts "Hobbies : #{arr[newIndex][:hobbies]}, Height: #{arr[newIndex][:height]}, from #{arr[newIndex][:country]} ".center(60)    
+         puts "(#{arr[newIndex][:cohort]} cohort)".center(60)
+         puts "Hobbies : #{arr[newIndex][:hobbies]}, Height: #{arr[newIndex][:height]}, from #{arr[newIndex][:country]} ".center(60)
          puts " -----------------------------------------------------------"
-           countStudent = countStudent - 1 
-           index = index + 1    
-         end 
-       end 
-         else 
-           return []          
-       end 
-     end    
+           countStudent = countStudent - 1
+           index = index + 1
+         end
+       end
+         else
+           return []
+       end
+     end
 
   # dipslay only students with the correct first letter + reject students with name > 12 characters
   #  return @students.select{ |x| x[:name][0].downcase == letter.downcase }
   #  .reject{ |x| x[:name].length > 12}
-  
-end 
+
+end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students" if @students.count > 1
   puts "Overall, we have #{@students.count} great student"  if @students.count == 1
   puts "NO STUDENT has been added to the list"  if @students.count == 0
-end 
+end
 
 def save_students
   file = File.open("students.csv", "w")
-  
+
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
-  end 
-  file.close   
-end 
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
 
 interactive_menu
 
 # students = input_students
-# 
-# 
+#
+#
 # if students.count > 0
-# print_header 
+# print_header
 # print(students)
-# end 
-# 
+# end
+#
 # print_footer(students)
-
-
